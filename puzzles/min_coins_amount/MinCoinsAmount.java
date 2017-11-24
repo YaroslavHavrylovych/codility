20656ea77436ea3898dda97540d7c04aa7fd29de
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2017-11-24 23:04:43 +0200

    minimum coins amount task

---
+++ puzzles/min_coins_amount/MinCoinsAmount.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+/**
+ * Check README to find description.
+ */
+public class MinCoinsAmount {
+    private static final int[] COINS = new int[] {1, 3, 4, 5, 10};
+    private static final int S = 37;
+
+    public int minAmount(int[] coins, int s) {
+        int[] coinsTable = new int[37];
+        for(int i = 0; i < coinsTable.length; i++) 
+            coinsTable[i] = Integer.MAX_VALUE;
+        return minAmount(coins, s, 0, coinsTable);
+    }
+
+    private int minAmount(int[] coins, int s, int current, int[] coinsTable) {
+        if(current == coinsTable.length) return coinsTable[current - 1];
+        for(int i = 0; i < coins.length; i++) {
+            if(coins[i] == current + 1) coinsTable[current] = 1;
+            else if(current - coins[i] < 0) continue;
+            else if(coinsTable[current - coins[i]] < coinsTable[current])
+                coinsTable[current] = coinsTable[current - coins[i]] + 1;
+        }
+        return minAmount(coins, s, current + 1, coinsTable);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Available coins: " + Arrays.toString(COINS));
+        System.out.println("Sum to achieve: " + S);
+        System.out.println("Minimum amount of coins: " 
+                + new MinCoinsAmount().minAmount(COINS, S));
+
+    }
+}
+
---
+++ puzzles/min_coins_amount/README.md
@@ -0,0 +1,10 @@
+## Minimum coins amount
+
+[Original](https://www.topcoder.com/community/data-science/data-science-tutorials/dynamic-programming-from-novice-to-advanced/)
+
+Preconditions: 
+Given a list of N coins, their values (V1, V2, … , VN), and the total sum S.
+
+TODO:  Find the minimum number of coins the sum of which is S (we can use as
+many coins of one type as we want), or report that it’s not possible to select
+coins in such a way that they sum up to S. 
