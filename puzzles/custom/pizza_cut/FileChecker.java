fa5c3ad4bff8e1cfdade81a5c17ffbe7a0296121
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2018-02-18 20:36:03 +0200

    worst PizzaSlicer ever

---
+++ puzzles/custom/pizza_cut/FileChecker.java
@@ -0,0 +1,85 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+public class FileChecker {
+    public void readPizzaFile(String fileName) throws IOException {
+        List<String> content = Files.lines(Paths.get(fileName))
+            .collect(Collectors.toList());
+        int slicesAmount = Integer.parseInt(content.remove(0));
+        assert(content.size() == slicesAmount);
+        Set<SlicePoint> stPt = new HashSet<>();
+        for(int i = 0; i < slicesAmount; i++)
+            for(SlicePoint pt: parsePoints(content.get(i))) {
+                if(stPt.contains(pt)) {
+                    System.out.println("Point " + pt + "is in reuse by slice "
+                            + content.get(i));
+                    return;
+                }
+            }
+        System.out.println("All slices are valid");
+    }
+
+    private class SlicePoint {
+        private int maxOfAbsAndOrd = 1001;
+        private int x;
+        private int y;
+
+        private SlicePoint(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Integer.hashCode(x * maxOfAbsAndOrd + y);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(obj == null || !(obj instanceof SlicePoint)) return false;
+            SlicePoint pt = (SlicePoint) obj;
+            return pt.x == x && pt.y == y;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + x + "," + y + "]";
+        }
+    }
+
+    public List<SlicePoint> parsePoints(String square) {
+        String[] splitted = square.split(" ");
+        int x1 = Integer.parseInt(splitted[0]);
+        int y1 = Integer.parseInt(splitted[1]);
+        int x2 = Integer.parseInt(splitted[2]);
+        int y2 = Integer.parseInt(splitted[3]);
+        int dX = x2 - x1 + 1;
+        int dY = y2 - y1 + 1;
+        List<SlicePoint> pts = new ArrayList<>(dX * dY);
+        for(int i = x1; i <= x2; i++)
+            for(int j = y1; j  <= y2; j++)
+                pts.add(new SlicePoint(i, j));
+        return pts;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.out.println("You need to pass file name to verify or");
+            return;
+        }
+        String arg = args[0];
+        File file = new File(arg);
+        if(!file.exists() || file.isDirectory()) {
+            System.out.println("File '" + arg + "' doesn't exist, or it's a"
+                   + " directory");
+        }
+        new FileChecker().readPizzaFile(arg);
+    }
+}
--- puzzles/custom/pizza_cut/Slicer.java
+++ puzzles/custom/pizza_cut/Slicer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -10,11 +11,10 @@ public class Slicer {
         int[][][] slicerTmp = new int[r][][];
         for(int i = 0; i < r; i++) slicerTmp[i] = new int[c][6]; 
         int[] ind = new int[2];
-        //TODO why r but not r-1
-        ind[0] = r;
+        ind[0] = r - 2;
         ind[1] = c;
         runSlicer(slicerTmp, ind, pizza, l, h);
-        System.out.println("scores " + slicerTmp[0][0][0]);
+        System.out.println("Scores: " + slicerTmp[0][0][0]);
         return pizzaToSlicesConvertor(slicerTmp, r, c);
     }
 
@@ -29,6 +29,7 @@ public class Slicer {
      */
     private void runSlicer(int[][][] slicerTmp, int[] ind,
             char[][] pizza, int l, int h) {
+        int progress = 0;
         while(ind[0] >= 0 && ind[1] >= 0) {
             for(int i = ind[0]; i < ind[0] + h; i++) {
                 for(int j = ind[1]; j < ind[1] + h; j++) {
@@ -39,6 +40,11 @@ public class Slicer {
                     updateMax(slicerTmp, ind[0], ind[1], i, j);
                 }
             }
+            float currentPr = (ind[1] * 1f) / pizza[0].length * 100f;
+            if(progress != (int) currentPr) {
+                progress = (int) currentPr;
+                System.out.println("Progress " + progress + "%");
+            }
             if(ind[0] == 0 && ind[1] == 0) return;
             if(ind[0] == 0) {
                 ind[0] = slicerTmp.length;
@@ -48,16 +54,20 @@ public class Slicer {
         }
     }
 
-    private void updateMax(int[][][] slicerTmp, int i1, int j1, int i2, int j2) {
+    private void updateMax(int[][][] slicerTmp, int i1, int j1,
+            int i2, int j2) {
         int width = j2 - j1 + 1;
         int height = i2 - i1 + 1;
         int maxC = 0;
         int maxX = 0;
         int maxY = 0;
         int val = 0;
+        //search for the best slice at the moment, with verification that
+        //other slices wouldn't be intersect.
         for(int j = j2; j < slicerTmp[0].length; j++) {
             for(int i = 0; i < slicerTmp.length; i++) {
-                if(!isOuterPassedValue(slicerTmp, j1, i1, width, height, i, j))
+                if(!isSlicesNotIntersect(slicerTmp, j1, i1, width,
+                            height, i, j))
                     continue;
                 val = slicerTmp[i][j][0];
                 if(val > maxC) {
@@ -79,7 +89,18 @@ public class Slicer {
         }
     }
 
-    private boolean isOuterPassedValue(int[][][] slicerTmp, int x, int y,
+    private boolean isSlicesNotIntersect(int[][][] slicerTmp, int x, int y,
+            int width, int height, int i, int j) {
+        if(i >= slicerTmp.length || j >= slicerTmp[i].length) return false;
+        if(j >= x + width) return true;
+        if(j == x + width && i >= y + width) return true;
+        if(!isSliceNotIntersect(slicerTmp, x, y, width, height, i, j))
+                return false;
+        return isSlicesNotIntersect(slicerTmp, x, y, width, height,
+                slicerTmp[i][j][3], slicerTmp[i][j][4]);
+    }
+
+    private boolean isSliceNotIntersect(int[][][] slicerTmp, int x, int y,
             int width, int height, int i, int j) {
         if(x >= j && y >= i) return false;
         if((j >= x && i >= y) && (j < x + width && i < y + height)) 
@@ -107,9 +128,11 @@ public class Slicer {
         List<int[]> slices = new ArrayList<>();
         int nextI = pizza[0][0][4];
         int nextJ = pizza[0][0][5];
-        int currentI = 0;
-        int currentJ = 0;
-        while(!(nextI == 0 && nextJ == 0)) {
+        int currentI = -1;
+        int currentJ = -1;
+        while(!(currentI == 0 && currentJ == 0)) {
+            currentI = currentI == -1 ? 0 : currentI;
+            currentJ = currentJ == -1 ? 0 : currentJ;
             int[] slice = new int[4];
             slice[0] = currentI;
             slice[1] = currentJ;
@@ -125,11 +148,23 @@ public class Slicer {
     }
 
     public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            System.out.println("You need to pass file name to verify or");
+            return;
+        }
+        String arg = args[0];
+        File file = new File(arg + ".in");
+        if(!file.exists() || file.isDirectory()) {
+            System.out.println("File '" + arg + "' doesn't exist, or it's a"
+                   + " directory");
+        }
         PizzaReader pr = new PizzaReader();
-        pr.readPizzaFile("example.in");
+        pr.readPizzaFile(arg + ".in");
         List<int[]> slices = new Slicer().runSlicer(pr.getPizza(), pr.getL(),
                 pr.getH(), pr.getR(), pr.getC());
         PizzaWriter pw = new PizzaWriter();
-        pw.writePizza("example.out", slices);
+        pw.writePizza(arg + ".out", slices);
+        System.out.println("Checking result file...");
+        new FileChecker().readPizzaFile(arg + ".out");
     }
 }
