d24c19b929c52f1aaaa4c86a758061e9d905ad8b
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2018-08-20 23:23:55 +0200

    Jewellery problem solved in a brute way

--- puzzles/dynamic_programming/jewellery/Jewellery.java
+++ puzzles/dynamic_programming/jewellery/Jewellery.java
@@ -1,142 +1,86 @@
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.stream.IntStream;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Jewellery {
-    public int calculatePossibleOutcomes(int[] jewellery) {
+    public long splitJewellery(int[] jewellery) {
         Arrays.sort(jewellery);
-        System.out.println("Jewellery: "  + Arrays.toString(jewellery));
-        return calculateCombinations(new HashMap<>(), jewellery, 0, 1);
+        println(Arrays.toString(jewellery));
+        long last = 1 << jewellery.length;
+        Map<Integer, List<JewelleryDesc>> results = new HashMap<>();
+        for(long i = 1; i < last; i++)
+            calculateDesc(i, jewellery, results);
+        return calcualteOutcomes(jewellery, results);
     }
 
-    /**
-     * Calculate amount of combinations.
-     * <br/>
-     * Each combination presented as a sum. 
-     * Sum - a key in `sums` argument.
-     * Value in sums argument represents amount of such sums (different
-     * combinations with same sum).
-     * <br/>
-     *
-     * @param sums map which contains existing sum as a key and amount
-     * of such sums as a value
-     * @param jewellery jewellery values. Each jewellery value only once
-     * can be used in combination
-     * @param maxValue the biggest key in sums
-     * @param startInd start index marks the smallest index in jewellery
-     * array which we can use in combinations
-     *
-     * @return amount of combinations which can be generated for each
-     * possible sums key.
-     */
-    private int calculateCombinations(Map<Integer, Integer> sums,
-            int[] jewellery, int maxValue, int startInd) {
-        if(startInd == jewellery.length) return 0;
-        int val = jewellery[startInd - 1];
-        maxValue += maxValue + val;
-        sums = updateSums(sums, val);
-        System.out.println();
-        int comb = calculateCombinations(sums, jewellery, maxValue, 
-                startInd, 0);
-        System.out.println("At the moment combinations " + startInd 
-                + ": " + comb);
-        System.out.println("start value: " + jewellery[startInd]);
-        System.out.println("At the moment sums: " 
-                + Arrays.toString(sums.keySet().toArray()));
-        System.out.println("At the moment vals: " 
-                + Arrays.toString(sums.values().toArray()));
-        //int multiplier = calculateMiltiplierForCombination(jewellery,
-                //startInd);
-        System.out.println("mult=" + multiplier);
-        return  comb + calculateCombinations(sums, jewellery,
-                maxValue, startInd + 1);
+    private void calculateDesc(long val, int[] jewellery,
+            Map<Integer, List<JewelleryDesc>> results) {
+        long current = 1;
+        int i = 0;
+        int first = -1;
+        int last = i;
+        int sum = 0;
+        while(current <= val) {
+            if((current & val) == current) {
+                sum += jewellery[i];
+                last = i;
+                first = first == -1 ? i : first;
+            }
+            i++;
+            current = current << 1;
+        }
+        List<JewelleryDesc> sums = results.get(sum);
+        sums = sums == null ? new ArrayList<JewelleryDesc>() : sums;
+        sums.add(new JewelleryDesc(first, last, val));
+        results.put(sum, sums);
     }
 
-    /** Calculate amount of possible combinations (look combination in math) */
-    private int calculateMiltiplierForCombination(int[] jewellery,
-            int startInd) {
-        int combStartInd = startInd;
-        while(combStartInd >= 0) {
-            if(jewellery[startInd] == jewellery[combStartInd]) combStartInd--;
-            else break;
+    private int calcualteOutcomes(int[] jewellery,
+            Map<Integer, List<JewelleryDesc>> results) {
+        int res = 0;
+        for(Integer key: results.keySet()) {
+            List<JewelleryDesc> jewelleryDesc = results.get(key);
+            for(int i = 0; i < jewelleryDesc.size(); i++) {
+                for(int j = i+1; j < jewelleryDesc.size(); j++) {
+                    long val1 = jewelleryDesc.get(i).val;
+                    long val2 = jewelleryDesc.get(j).val;
+                    int highest1 = jewellery[jewelleryDesc.get(i).highestOne];
+                    int highest2 = jewellery[jewelleryDesc.get(j).highestOne];
+                    int lowest1 = jewellery[jewelleryDesc.get(i).lowestOne];
+                    int lowest2 = jewellery[jewelleryDesc.get(j).lowestOne];
+                    if(highest1 <= lowest2 && (val1 & val2) == 0) {
+                        if(highest2 == lowest1) res++;
+                        res++;
+                    }
+                }
+            }
         }
-        combStartInd++;
-        int combEndInd = startInd;
-        while(combEndInd < jewellery.length) {
-            if(jewellery[startInd] == jewellery[combEndInd]) combEndInd++;
-            else break;
-        }
-        combEndInd--;
-        int n = combEndInd - combStartInd + 1;
-        int k = startInd - combStartInd + 1;
-        System.out.println("multiplier n, k " + n + ", " + k);
-        return fact(n) / (fact(k) * fact(n - k));
+        return res;
     }
 
-    private int fact(int n) {
-        int f = 1;
-        for(int i = 1; i <= n; i++) {
-            f *= i;
-        }
-        return f;
-    }
-
-    /**
-     * Calculates new sums if we add one more value to the sequence.
-     *
-     * @param sums old sums values
-     * @param jewellery new value to add to the sequence
-     *
-     * @return all possible sums of the collection with appended
-     * jewellery value.
-     */
-    private Map<Integer, Integer> updateSums(Map<Integer, Integer> sums,
-            int jewellery) {
-        Map<Integer, Integer> newSums = new HashMap<>(sums);
-        //feed newSum with (oldVal+jewellery)
-        for(Map.Entry<Integer, Integer> entry: sums.entrySet()) {
-            int sum = entry.getKey();
-            sum += jewellery;
-            Integer amount = sums.get(sum);
-            newSums.put(sum, entry.getValue() 
-                    + (amount == null ?  0 : amount));
-        }
-        //add to newSum values which can't be represented as (oldVal+jewellery)
-        for(Map.Entry<Integer, Integer> entry: sums.entrySet()) {
-            Integer amount = newSums.get(entry.getKey());
-            if(amount == null) newSums.put(entry.getKey(), entry.getValue());
-        }
-        //add jewellery itself
-        if(newSums.get(jewellery) == null) newSums.put(jewellery, 1);
-        else newSums.put(jewellery, newSums.get(jewellery) + 1);
-        return newSums;
-    }
-
-    /**
-     * Very similar to {@link calculateCombinations} with startInd param,
-     * the difference is that this method has current sum value and current
-     * index we must use in our combination.
-     */
-    private int calculateCombinations(Map<Integer, Integer> sums,
-            int[] jewellery, int maxValue, int currentInd, int currentSum) {
-        currentSum += jewellery[currentInd];
-        if(currentSum > maxValue) return 0; //first of right > sum in the left
-        Integer amount = sums.get(currentSum);
-        if(amount == null) amount = 0;
-        else {System.out.println("ind=" + currentInd + ",currentSum=" + currentSum);}
-        for(int i = currentInd + 1; i < jewellery.length; i++)
-            amount += calculateCombinations(sums, jewellery, maxValue, 
-                    i, currentSum);
-        return amount;
+    private void println(String str) {
+        System.out.println(str);
     }
 
     public static void main(String[] args) {
-        int[] jewellery = new int[] {1,1,1};
-        System.out.println("Possible outcomes: "  + new Jewellery()
-                .calculatePossibleOutcomes(jewellery));
+        //int[] jewellery = new int[] {1,2,5,3,4,5};
+        //int[] jewellery = new int[] {1,2,5,3,4};
+        int[] jewellery = new int[] {7,7,8,9,10,11,1,2,2,3,4,5,6};
+        System.out.println("Possible outcomes: "  
+                + new Jewellery().splitJewellery(jewellery));
+    }
+
+    private class JewelleryDesc {
+        int lowestOne;
+        int highestOne;
+        long val;
+
+        private JewelleryDesc(int lowestOne, int highestOne, long val) {
+            this.lowestOne = lowestOne;
+            this.highestOne = highestOne;
+            this.val = val;
+        }
     }
 }
---
+++ puzzles/dynamic_programming/jewellery/README.md
@@ -0,0 +1,26 @@
+## Highway Billboard Problem
+
+[Original](https://community.topcoder.com/stat?c=problem_statement&pm=1166&rd=4705)
+
+Preconditions: 
+
+You have been given a list of jewelry items that must be split amongst
+two people: Frank and Bob. Frank likes very expensive jewelry.
+Bob doesn't care how expensive the jewelry is, as long as he gets a lot of
+jewelry. Based on these criteria you have devised the following policy:
+
+1) Each piece of jewelry given to Frank must be valued greater than or equal
+to each piece of jewelry given to Bob. In other words, Frank's least expensive
+piece of jewelry must be valued greater than or equal to Bob's most expensive
+piece of jewelry.
+
+2) The total value of the jewelry given to Frank must exactly equal the total
+value of the jewelry given to Bob.
+
+3) There can be pieces of jewelry given to neither Bob nor Frank.
+
+4) Frank and Bob must each get at least 1 piece of jewelry.
+
+TODO: Determine the number of different ways
+you can allocate the jewelry to Bob and Frank following the above policy.
+
