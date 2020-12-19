e4fceca12e8d8004cb716653cc0a42e46b72c05b
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2020-12-19 18:23:58 +0200

    Leetcode 1015. Math. Smallest Integer Divisible by K.

---
+++ math/SmallestIntegerDivisibleByK/kotlin/Solution.kt
@@ -0,0 +1,37 @@
+import java.util.LinkedList
+
+/**
+ * Given a positive integer K, you need to find the length of the smallest 
+ * positive integer N such that N is divisible by K, 
+ * and N only contains the digit 1.
+ * Return the length of N. If there is no such N, return -1.
+ * Note: N may not fit in a 64-bit signed integer.
+ * <br>
+ * https://leetcode.com/problems/smallest-integer-divisible-by-k/
+ */
+fun smallestRepunitDivByK(K: Int): Int {
+    if (K % 2 == 0 || K % 5 == 0) return -1
+    var l = 1L
+    var n = 1
+    while (l < K) {
+        n++
+        l = l * 10 + 1
+    }
+    l %= K
+    while (l != 0L) {
+        l = (((l % K) * (10 % K)) % K + 1) % K
+        if (n == Int.MAX_VALUE) return -1
+        n++
+    }
+    return n
+}
+
+fun main() {
+    print("Smallest Integer Divisible by K: ")
+    var a = true
+    a = a && smallestRepunitDivByK(1) == 1
+    a = a && smallestRepunitDivByK(3) == 3
+    a = a && mallestRepunitDivByK(7) == 6
+    a = a && mallestRepunitDivByK(12371) == 1012 
+    println(if(a) "SUCCESS" else "FAIL")
+} 
